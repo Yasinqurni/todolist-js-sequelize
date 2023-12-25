f76ffd49bf213a2013c91a2a278cde1c169efc81
yasin <yalqurni@gmail.com>
2023-12-25 22:24:42 +0700

    initial commit

---
+++ src/repository/model/todolist_model.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize")
+const sequelize = require("../config/config")
+
+class Todolist extends Model {
+}
+
+cart.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      }, 
+      tags: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'user_id'
+      },
+      body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+  },
+  {
+    sequelize: sequelize,
+    timestamps: true,
+    paranoid: true,
+    underscored: true,
+    deletedAt: 'deleted_at',
+    updatedAt: 'updated_at',
+    createdAt: 'created_at',
+  },
+)
+
+module.exports = Todolist
---
+++ src/repository/todolist_repository.js
@@ -0,0 +1,10 @@
+const { Todolist } =  require('./model/todolist_model.js')
+class TodolistRepository {
+    // constructor(todolistModel) {
+    //     this.todolistModel = todolistModel
+    // }
+
+    async CreateTodolist(todolist) {
+        Todolist.
+    }
+}
